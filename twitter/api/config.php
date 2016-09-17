9338ff24fe8a21f37f4aa6d76148f6f0f8861491
jpstearns <jared.patrick.stearns@gmail.com>
2016-09-17 16:01:52 -0400

    bring in theme

---
+++ twitter/api/config.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'U5otC74KsCDGVlmbz61gObrhO');
+    define('CONSUMER_SECRET', '3NRGOKD9871WdqxWEijHczxeS1IZ6OPGfLhboMqCA2xtvIhzmy');
+
+    // User Access Token
+    define('ACCESS_TOKEN', '3092046298-T8tEpJeNa7LVxknoeFleu7qU1xUrwZSGCq0iR0B');
+    define('ACCESS_SECRET', 'IyeisKvwKBqh8sDr2X4xcjov4ln7txSuIwao9iONZccZe');
+	
+	// Cache Settings
+	define('CACHE_ENABLED', false);
+	define('CACHE_LIFETIME', 3600); // in seconds
+	define('HASH_SALT', md5(dirname(__FILE__)));
